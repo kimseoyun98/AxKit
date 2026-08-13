d053589beec5e6eca6e1277dc3a7fca0924d5ede
kimseoyun98 <seoyunkim@example.com>
2026-08-14 00:32:44 +0900

    feat: track docs/src/components/ui/layout.tsx

---
+++ docs/src/components/ui/layout.tsx
@@ -0,0 +1,20 @@
+/**
+ * @file ui:layout
+ * @requires @seed-design/react@^2.0.0
+ * @requires @seed-design/css@^2.0.0
+ **/
+
+export {
+  Box,
+  Flex,
+  Grid,
+  VStack,
+  HStack,
+  Float,
+  type BoxProps,
+  type FlexProps,
+  type GridProps,
+  type VStackProps,
+  type HStackProps,
+  type FloatProps,
+} from "@seed-design/react";
