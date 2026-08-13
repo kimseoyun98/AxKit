c3e52f75a92d86ace75e082386367331eb8cdc0e
kimseoyun98 <seoyunkim@example.com>
2026-08-14 01:02:58 +0900

    fix: add default fallback monochrome icon mapping for ContentPlaceholder types (profile, image, document, chat)

--- docs/src/components/ui/content-placeholder.tsx
+++ docs/src/components/ui/content-placeholder.tsx
@@ -4,19 +4,33 @@
  * @requires @seed-design/css@^2.0.0
  **/
 
+import IconDocumentFill from "@karrotmarket/react-monochrome-icon/IconDocumentFill";
+import IconDot3HorizontalChatbubbleLeftFill from "@karrotmarket/react-monochrome-icon/IconDot3HorizontalChatbubbleLeftFill";
+import IconPersonFill from "@karrotmarket/react-monochrome-icon/IconPersonFill";
+import IconPictureFill from "@karrotmarket/react-monochrome-icon/IconPictureFill";
 import { ContentPlaceholder as SeedContentPlaceholder } from "@seed-design/react";
 import * as React from "react";
 
-export interface ContentPlaceholderProps extends SeedContentPlaceholder.RootProps {}
+export interface ContentPlaceholderProps extends SeedContentPlaceholder.RootProps {
+  type?: "default" | "profile" | "image" | "document" | "chat" | string;
+}
+
+const defaultIcons: Record<string, React.ReactNode> = {
+  profile: <IconPersonFill />,
+  image: <IconPictureFill />,
+  document: <IconDocumentFill />,
+  chat: <IconDot3HorizontalChatbubbleLeftFill />,
+};
 
 /**
  * @see https://seed-design.io/react/components/content-placeholder
  */
 export const ContentPlaceholder = React.forwardRef<HTMLDivElement, ContentPlaceholderProps>(
-  ({ children, ...props }, ref) => {
+  ({ children, type, ...props }, ref) => {
+    const icon = children || (type && defaultIcons[type] ? defaultIcons[type] : <IconPictureFill />);
     return (
-      <SeedContentPlaceholder.Root {...props} ref={ref}>
-        <SeedContentPlaceholder.Asset>{children}</SeedContentPlaceholder.Asset>
+      <SeedContentPlaceholder.Root type={type} {...props} ref={ref}>
+        <SeedContentPlaceholder.Asset>{icon}</SeedContentPlaceholder.Asset>
       </SeedContentPlaceholder.Root>
     );
   },
